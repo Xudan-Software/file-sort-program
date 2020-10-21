f16b5f99f952577bca66a95c200954d8fb12b097
Jordan Gillard <jordan-gillard@outlook.com>
2020-10-21 10:46:18 -0500

    minor

---
+++ src/RecordTest.java
@@ -0,0 +1,53 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Holds a single record
+ * 
+ * @author CS Staff
+ * @version 2020-10-15
+ */
+public class RecordTest {
+
+    private byte[] aBite;
+
+
+    /**
+     * The setup for the tests
+     */
+    @Before
+    public void setUp() {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES + Double.BYTES);
+        buffer.putLong(7);
+        buffer.putDouble(8, 1);
+        aBite = buffer.array();
+    }
+
+
+    /**
+     * Tests the first constructor
+     */
+    @Test
+    public void testConstruct1() {
+        Record rec = new Record(aBite);
+        assertEquals((double)1, rec.getKey(), 0.00);
+        assertEquals(aBite, rec.getCompleteRecord());
+        assertTrue(rec.toString().equals("1.0"));
+    }
+    
+    /**
+     * Tests the first constructor
+     */
+    @Test
+    public void testCompareTo() {
+        Record rec = new Record(aBite);
+        Record recToBeCompared = new Record(aBite);
+        assertEquals(rec.compareTo(recToBeCompared), 0);
+    }
+
+}
