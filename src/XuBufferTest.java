319bdcff71bef70b34a7cf5a8b456bc6a1a73cdb
Jordan Gillard <jordan-gillard@outlook.com>
2020-10-28 10:08:37 -0500

    docs: Adds JavaDocs to test methods in XuBufferTest

--- src/XuBufferTest.java
+++ src/XuBufferTest.java
@@ -20,6 +20,7 @@ public class XuBufferTest {
 
     /**
      * make 16 byte-long array that contains 8 long and 8 double
+     *
      * @param l long value
      * @param d double value
      * @return a byte array contains long and double values
@@ -36,6 +37,7 @@ public class XuBufferTest {
      * set up random access file for write and read from Xubuffer
      * full buffer is buffer with full size (5) amout of data
      * empty buffer is an empty buffer with no data
+     *
      * @throws FileNotFoundException exception for random access file
      */
     @Before public void setUp() throws FileNotFoundException {
@@ -88,6 +90,7 @@ public class XuBufferTest {
 
     /**
      * test write to file from an empty buffer
+     *
      * @throws IOException random access file exception
      */
     @Test public void testWriteToFileEmptyXuBuffer() throws IOException {
@@ -99,6 +102,7 @@ public class XuBufferTest {
 
     /**
      * test write to file from a full buffer
+     *
      * @throws IOException random access file exception
      */
     @Test public void testWriteToFileFullBuffer() throws IOException {
@@ -110,8 +114,10 @@ public class XuBufferTest {
         Assert.assertArrayEquals(fullBuffer.toByteArray(), raBytes);
     }
 
+
     /**
      * test write to file from an almost full buffer that contains 9 values
+     *
      * @throws IOException random access file exception
      */
     @Test public void testWriteToFileAlmostFullBuffer() throws IOException {
@@ -135,48 +141,97 @@ public class XuBufferTest {
 
     }
 
-    @Test public void testGetLastXBytesWithEmptyBuffer(){
-        byte[] bytesToReturn=emptyBuffer.getLastXBytes(16);
-       Assert.assertArrayEquals(new byte[]{},bytesToReturn);
+
+    /**
+     * Test getting the last bytes from an empty buffer returns an empty byte
+     * array.
+     */
+    @Test public void testGetLastXBytesWithEmptyBuffer() {
+        byte[] bytesToReturn = emptyBuffer.getLastXBytes(16);
+        Assert.assertArrayEquals(new byte[] {}, bytesToReturn);
 
     }
 
-    @Test public void testGetLastXBytesWithFullBuffer(){
-        byte[] bytesToReturn=fullBuffer.getLastXBytes(16);
-        Assert.assertArrayEquals(Arrays.copyOfRange(fullBuffer.toByteArray(),64,80),bytesToReturn);
+
+    /**
+     * Tests getting the last bytes with a full buffer returns the correct
+     * array.
+     */
+    @Test public void testGetLastXBytesWithFullBuffer() {
+        byte[] bytesToReturn = fullBuffer.getLastXBytes(16);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(fullBuffer.toByteArray(), 64, 80),
+            bytesToReturn);
 
     }
 
-    @Test public void testGetLastXBytesWithAlmostFullBuffer(){
+
+    /**
+     * Tests getting the last bytes with an almost full buffer returns the
+     * correct array.
+     */
+    @Test public void testGetLastXBytesWithAlmostFullBuffer() {
         XuBuffer threeBuffer = new XuBuffer(5 * 16);
         threeBuffer.put(new Record(makeRedArray(0L, 0D)).getCompleteRecord());
         threeBuffer.put(new Record(makeRedArray(1L, 1.0D)).getCompleteRecord());
         threeBuffer.put(new Record(makeRedArray(2L, 2.0D)).getCompleteRecord());
-        byte[] bytesToReturn=threeBuffer.getLastXBytes(16);
-        Assert.assertArrayEquals(Arrays.copyOfRange(threeBuffer.toByteArray(),32,48),bytesToReturn);
+        byte[] bytesToReturn = threeBuffer.getLastXBytes(16);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(threeBuffer.toByteArray(), 32, 48),
+            bytesToReturn);
     }
 
-    @Test public void testGetFirstXBytesWithEmptyBuffer(){
-        byte[] bytesToReturn=emptyBuffer.popFirstXBytes(16);
-        Assert.assertArrayEquals(new byte[]{},bytesToReturn);
+
+    /**
+     * Test popping the first bytes from an empty buffer returns an empty byte
+     * array.
+     */
+    @Test public void testPopFirstXBytesWithEmptyBuffer() {
+        byte[] bytesToReturn = emptyBuffer.popFirstXBytes(16);
+        Assert.assertArrayEquals(new byte[] {}, bytesToReturn);
     }
 
-    @Test public void testGetFirstXBytesWithFullBuffer(){
-        byte[] bytesToReturn=fullBuffer.popFirstXBytes(32);
-        Assert.assertArrayEquals(Arrays.copyOfRange(fullBuffer.toByteArray(),0,32),bytesToReturn);
+
+    /**
+     * Tests that popping the first bytes of a full buffer returns the correct
+     * byte array.
+     */
+    @Test public void testPopFirstXBytesWithFullBuffer() {
+        byte[] bytesToReturn = fullBuffer.popFirstXBytes(32);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(fullBuffer.toByteArray(), 0, 32), bytesToReturn);
     }
 
-    @Test public void testGetFirstXBytesWithFullBufferMultiTimes(){
-        byte[] bytesToReturn1=fullBuffer.popFirstXBytes(16);
-        byte[] bytesToReturn2=fullBuffer.popFirstXBytes(16);
-        Assert.assertArrayEquals(Arrays.copyOfRange(fullBuffer.toByteArray(),16,32),bytesToReturn2);
+
+    /**
+     * Tests that using popFirstXBytes multiple times continues to return
+     * the correct byte array for each call.
+     */
+    @Test public void testPopFirstXBytesWithFullBufferMultiTimes() {
+        fullBuffer.popFirstXBytes(16);
+        byte[] bytesToReturn2 = fullBuffer.popFirstXBytes(16);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(fullBuffer.toByteArray(), 16, 32),
+            bytesToReturn2);
     }
-    @Test public void testPopFirstXBytesAndLastBytes(){
-        byte[] bytesToReturn1=fullBuffer.popFirstXBytes(16);
-        byte[] bytesToReturn2=fullBuffer.popFirstXBytes(16);
-        byte[] lastBytesToReturn=fullBuffer.getLastXBytes(16);
-        Assert.assertArrayEquals(Arrays.copyOfRange(fullBuffer.toByteArray(),0 ,16),bytesToReturn1);
-        Assert.assertArrayEquals(Arrays.copyOfRange(fullBuffer.toByteArray(),16,32),bytesToReturn2);
-        Assert.assertArrayEquals(Arrays.copyOfRange(fullBuffer.toByteArray(),64,80),lastBytesToReturn);
+
+
+    /**
+     * Tests that using popFirstXBytes and getLastXBytes work together without
+     * interference between the two methods.
+     */
+    @Test public void testPopFirstXBytesAndLastBytes() {
+        byte[] bytesToReturn1 = fullBuffer.popFirstXBytes(16);
+        byte[] bytesToReturn2 = fullBuffer.popFirstXBytes(16);
+        byte[] lastBytesToReturn = fullBuffer.getLastXBytes(16);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(fullBuffer.toByteArray(), 0, 16),
+            bytesToReturn1);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(fullBuffer.toByteArray(), 16, 32),
+            bytesToReturn2);
+        Assert.assertArrayEquals(
+            Arrays.copyOfRange(fullBuffer.toByteArray(), 64, 80),
+            lastBytesToReturn);
     }
 }
